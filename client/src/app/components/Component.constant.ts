65bc63e80b2c0bce5ce224ba4f30fdead2f6f63b
Unknown <pascal.chayer@polymtl.ca>
2021-04-25 20:58:25 -0400

    Succed Swap and TimeSwap

--- client/src/app/components/Component.constant.ts
+++ client/src/app/components/Component.constant.ts
@@ -1,1 +1,2 @@
 export const BEGIN_OUTPUT = 'Display Screen';
+export const IMAGE_NAME_LIST = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"]
--- client/src/app/components/photo-swap/photo-swap.component.ts
+++ client/src/app/components/photo-swap/photo-swap.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IMAGE_NAME_LIST } from '../Component.constant';
+import { interval, Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-photo-swap',
@@ -8,17 +12,34 @@ import { Component, OnInit } from '@angular/core';
 export class PhotoSwapComponent implements OnInit {
 
   path = '../../../assets/';
-  imageDisplay = 'img1.jpg';
-  src = this.path + this.imageDisplay;
+  imageDisplay = 0;
+  src = '';
+  subscription: Subscription;
+  intervalId: number;
+  timer = 3000
 
   constructor() { }
 
   ngOnInit(): void {
+    this.src = this.path + IMAGE_NAME_LIST[this.imageDisplay];
+    const source = interval(this.timer);
+    this.subscription = source.subscribe(val => this.onClick());
   }
 
-  onclick(): void {
-    console.log(this.imageDisplay);
-    this.imageDisplay = 'img2.jpg';
-    console.log(this.imageDisplay);
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+  }
+
+  onClick(): void {
+    var newIndex = this.imageDisplay;
+    while (newIndex === this.imageDisplay)
+    {
+      newIndex = Math.floor(Math.random() * IMAGE_NAME_LIST.length);
+    }
+    this.imageDisplay = newIndex;
+    this.src = this.path + IMAGE_NAME_LIST[this.imageDisplay];
+    this.subscription.unsubscribe();
+    const source = interval(this.timer);
+    this.subscription = source.subscribe(val => this.onClick());
   }
 }
