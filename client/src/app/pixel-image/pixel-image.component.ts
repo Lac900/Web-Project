14a6e04dc90fc43b0b5ef3104a8f1a23947b415d
lac900 <lac900@live.ca>
2022-02-23 09:54:08 -0500

    pixel-image canvas start

--- client/src/app/pixel-image/pixel-image.component.ts
+++ client/src/app/pixel-image/pixel-image.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pixel-image',
   templateUrl: './pixel-image.component.html',
   styleUrls: ['./pixel-image.component.scss']
 })
-export class PixelImageComponent implements OnInit {
+export class PixelImageComponent implements AfterViewInit {
+  @ViewChild('image') img: HTMLImageElement;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    console.log('ada');
+    const myCanvas = document.getElementById('originalCanvas') as HTMLCanvasElement;
+    const ctx = myCanvas.getContext('2d');
+    const imgTmp = new Image();
+    imgTmp.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Spyro.png/220px-Spyro.png';
+    imgTmp.onload = () => {
+      ctx.drawImage(imgTmp, 0, 0, 200, 200); // Or at whatever offset you like
+      console.log(imgTmp);
+    };
   }
 
 }
